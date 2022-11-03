4677765db63aca911faa3c56a700b572d3242988
Amit0129 <amitkumarnayak40@gmail.com>
2022-11-03 18:19:14 +0530

    Added:UC3_The Player Check For Option using Random Function

--- SnakeandLadder/SnakeandLadder/Program.cs
+++ SnakeandLadder/SnakeandLadder/Program.cs
@@ -15,6 +15,15 @@
             int diceVal = randon.Next(1, 7);
             Console.WriteLine("The value u ger from rolling the dice is {0} ", diceVal);
 
+            int checkOp = randon.Next(0, 3);
+            Console.WriteLine( checkOp);
+            switch (checkOp)
+            {
+                case 0: Console.WriteLine("There are No Play");break;
+                case 1: Console.WriteLine("Ladder");break;
+                case 2: Console.WriteLine("Snake");break;
+            }
+
 
 
 
