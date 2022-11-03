8e824d6a238dce1219950df01e561c3051fa36a3
Amit0129 <amitkumarnayak40@gmail.com>
2022-11-03 18:45:02 +0530

    Added:UC4UC4_Repeat Till The Player Reaches Winning Position 100

--- SnakeandLadder/SnakeandLadder/Program.cs
+++ SnakeandLadder/SnakeandLadder/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace SnakeandLadder
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace SnakeandLadder
 {
     internal class Program
     {
@@ -7,22 +9,55 @@
             Console.WriteLine("Welcome Snake and Ladder Game");
 
             int plearPosition = 0;
+            const int WIN_POSI = 100;
+            int playerRollCount = 0;
             Console.WriteLine("The starting position of the player is {0}", plearPosition);
 
 
-
-            Random randon = new Random();
-            int diceVal = randon.Next(1, 7);
-            Console.WriteLine("The value u ger from rolling the dice is {0} ", diceVal);
-
-            int checkOp = randon.Next(0, 3);
-            Console.WriteLine( checkOp);
-            switch (checkOp)
+            while (plearPosition < WIN_POSI)
             {
-                case 0: Console.WriteLine("There are No Play");break;
-                case 1: Console.WriteLine("Ladder");break;
-                case 2: Console.WriteLine("Snake");break;
-            }
+                playerRollCount++;
+
+
+                Random randon = new Random();
+                int diceVal = randon.Next(1, 7);
+                Console.WriteLine("The value u ger from rolling the dice is {0} ", diceVal);
+
+                int checkOp = randon.Next(0, 3);
+                Console.WriteLine(checkOp);
+
+               
+                    //plearPosition += diceVal;
+                    //Console.WriteLine("The Player PositionNow is {0} ", plearPosition);
+
+                    switch (checkOp)
+                    {
+                        case 0:
+                            Console.WriteLine("There are No Play");
+                            Console.WriteLine("Player Position is {0}", plearPosition);
+                            break;
+                        case 1:
+
+                            Console.WriteLine("Ladder");
+                            plearPosition += diceVal;
+                            Console.WriteLine("Player Position is {0}", plearPosition);
+
+                            break;
+                        case 2:
+                            Console.WriteLine("Snake");
+                            plearPosition -= diceVal;
+                            if (plearPosition < 0)
+                            {
+                                plearPosition = 0;
+                            }
+                            Console.WriteLine("Player Position is {0}", plearPosition);
+                            break;
+                    }
+
+                }
+            
+            
+        
 
 
 
