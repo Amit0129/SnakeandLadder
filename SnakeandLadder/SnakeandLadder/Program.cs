bffeeddd9a6bde55486d96e3d2a09c903878ed0d
Amit0129 <amitkumarnayak40@gmail.com>
2022-11-03 18:10:14 +0530

    Added:UC2_The player roll the dice to get number between 1 to 6

--- SnakeandLadder/SnakeandLadder/Program.cs
+++ SnakeandLadder/SnakeandLadder/Program.cs
@@ -7,12 +7,15 @@
             Console.WriteLine("Welcome Snake and Ladder Game");
 
             int plearPosition = 0;
-
-            Console.WriteLine("The Player Start from Position {0}",plearPosition);
-
+            Console.WriteLine("The starting position of the player is {0}", plearPosition);
 
 
-            
+
+            Random randon = new Random();
+            int diceVal = randon.Next(1, 7);
+            Console.WriteLine("The value u ger from rolling the dice is {0} ", diceVal);
+
+
 
 
 
